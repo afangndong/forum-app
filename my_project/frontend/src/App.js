30f68e8907e2213eba43121d5629663c5fd87fe4
afangndong <43615715+afangndong@users.noreply.github.com>
2024-10-09 15:32:02 +0200

    Update App.js
    Main Application for our messages

--- my_project/frontend/src/App.js
+++ my_project/frontend/src/App.js
@@ -1,1 +1,17 @@
+import React from 'react';
+import Login from './components/Login';
+import Signup from './components/Signup';
+import Forum from './components/Forum';
 
+function App() {
+    return (
+        <div>
+            <h1>Welcome to the Forum</h1>
+            <Signup />
+            <Login />
+            <Forum />
+        </div>
+    );
+}
+
+export default App;
