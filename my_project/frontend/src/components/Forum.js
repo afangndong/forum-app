f9f72d741db084186403ff432dde7af804e8e873
afangndong <43615715+afangndong@users.noreply.github.com>
2024-10-09 15:30:01 +0200

    Update Forum.js
    Forum Component to fetch messages in DB Messages

--- my_project/frontend/src/components/Forum.js
+++ my_project/frontend/src/components/Forum.js
@@ -1,1 +1,26 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
+function Forum() {
+    const [messages, setMessages] = useState([]);
+
+    useEffect(() => {
+        const fetchMessages = async () => {
+            const response = await axios.get('http://localhost:5000/api/messages');
+            setMessages(response.data);
+        };
+        fetchMessages();
+    }, []);
+
+    return (
+        <div>
+            {messages.map((msg) => (
+                <div key={msg.id}>
+                    <p>{msg.content}</p>
+                </div>
+            ))}
+        </div>
+    );
+}
+
+export default Forum;
