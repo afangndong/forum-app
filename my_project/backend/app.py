efbd3b58c562073f4bdfe5d95980138f9489cb47
afangndong <43615715+afangndong@users.noreply.github.com>
2024-10-09 15:26:49 +0200

    Update app.py
    main application

--- my_project/backend/app.py
+++ my_project/backend/app.py
@@ -1,1 +1,10 @@
+from flask import Flask
+from database import create_app
+from routes import api
 
+app = create_app()
+app.register_blueprint(api, url_prefix='/api')
+
+if __name__ == '__main__':
+    with app.app_context():
+        app.run(debug=True, host='0.0.0.0')
