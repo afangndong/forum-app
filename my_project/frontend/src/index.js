875191c373c8ba50f94eef719db2620a6d5dbc73
afangndong <43615715+afangndong@users.noreply.github.com>
2024-10-09 16:01:27 +0200

    Update index.js

--- my_project/frontend/src/index.js
+++ my_project/frontend/src/index.js
@@ -1,1 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App'; // Import your main App component
+import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
+import './index.css'; // Import your custom CSS if any
 
+// Render the App component into the root div in index.html
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
