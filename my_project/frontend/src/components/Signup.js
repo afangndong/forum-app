b9cc985c2d00e9eba96f65cf95ac4d24ce2f8014
afangndong <43615715+afangndong@users.noreply.github.com>
2024-10-09 15:33:10 +0200

    Update Signup.js
    Signup Component to create user in DB-User

--- my_project/frontend/src/components/Signup.js
+++ my_project/frontend/src/components/Signup.js
@@ -1,1 +1,23 @@
+import React, { useState } from 'react';
+import axios from 'axios';
 
+function Signup() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const response = await axios.post('http://localhost:5000/api/signup', { username, password });
+        alert(response.data.message);
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input type="text" placeholder="Username" onChange={(e) => setUsername(e.target.value)} />
+            <input type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
+            <button type="submit">Sign Up</button>
+        </form>
+    );
+}
+
+export default Signup;
