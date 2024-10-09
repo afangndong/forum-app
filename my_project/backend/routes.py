00df8583d87138cd37def94702b83c2e69dc6edd
afangndong <43615715+afangndong@users.noreply.github.com>
2024-10-09 15:26:10 +0200

    Update routes.py
    routes for each pages

--- my_project/backend/routes.py
+++ my_project/backend/routes.py
@@ -1,1 +1,33 @@
+from flask import Blueprint, request, jsonify
+from models import db, User, Message
 
+api = Blueprint('api', __name__)
+
+@api.route('/signup', methods=['POST'])
+def signup():
+    data = request.json
+    new_user = User(username=data['username'], password=data['password'])
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify({"message": "User created"}), 201
+
+@api.route('/login', methods=['POST'])
+def login():
+    data = request.json
+    user = User.query.filter_by(username=data['username']).first()
+    if user and user.password == data['password']:
+        return jsonify({"message": "Login successful"}), 200
+    return jsonify({"message": "Invalid credentials"}), 401
+
+@api.route('/post_message', methods=['POST'])
+def post_message():
+    data = request.json
+    new_message = Message(user_id=data['user_id'], content=data['content'])
+    db.session.add(new_message)
+    db.session.commit()
+    return jsonify({"message": "Message posted"}), 201
+
+@api.route('/messages', methods=['GET'])
+def get_messages():
+    messages = Message.query.all()
+    return jsonify([{"user_id": msg.user_id, "content": msg.content} for msg in messages]), 200
