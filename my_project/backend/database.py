59bc4a6631f8f2af76e50b50449fa9f7e7acdfbd
afangndong <43615715+afangndong@users.noreply.github.com>
2024-10-09 15:23:48 +0200

    Update database.py
    set up DB

--- my_project/backend/database.py
+++ my_project/backend/database.py
@@ -1,1 +1,11 @@
+from flask import Flask
+from models import db
 
+def create_app():
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+    app.config['SQLALCHEMY_BINDS'] = {
+        'messages': 'sqlite:///messages.db'
+    }
+    db.init_app(app)
+    return app
